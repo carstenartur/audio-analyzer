54973a693858cd19b7f0af1142685145e2d17be4
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-20 22:53:08 +0200

    Add stable deterministic audio diagnostic codes (#274)

---
+++ audio-dsp/src/main/java/org/hammer/audio/dsp/workflow/DeterministicAudioDiagnostics.java
@@ -0,0 +1,9 @@
+package org.hammer.audio.dsp.workflow;
+
+/** Stable diagnostic codes produced by the deterministic audio backend. */
+public interface DeterministicAudioDiagnostics {
+
+  String UNSUPPORTED_NODE = "UNSUPPORTED_NODE";
+  String INVALID_PARAMETER = "INVALID_PARAMETER";
+  String INVALID_TOPOLOGY = "INVALID_TOPOLOGY";
+}
