af30885e9cc7ae68d2d2a49fc93f6c7a56bfa55d
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-20 23:23:53 +0200

    Model deterministic artifact keys as utility type (#274)

--- audio-dsp/src/main/java/org/hammer/audio/dsp/workflow/DeterministicAudioArtifacts.java
+++ audio-dsp/src/main/java/org/hammer/audio/dsp/workflow/DeterministicAudioArtifacts.java
@@ -1,27 +1,31 @@
 package org.hammer.audio.dsp.workflow;
 
 /** Stable textual artifact keys emitted by the deterministic audio workflow backend. */
-public interface DeterministicAudioArtifacts {
+public final class DeterministicAudioArtifacts {
 
-  String BACKEND_MODE = "backendMode";
-  String BACKEND_VERSION = "backendVersion";
-  String OUTPUT_NODE_ID = "output.nodeId";
-  String OUTPUT_PORT_ID = "output.portId";
-  String OUTPUT_DIGEST_ALGORITHM = "output.digest.algorithm";
-  String OUTPUT_DIGEST_ENCODING = "output.digest.encoding";
-  String OUTPUT_DIGEST_SHA256 = "output.digest.sha256";
-  String OUTPUT_SAMPLE_RATE_HZ = "output.sampleRateHz";
-  String OUTPUT_CHANNELS = "output.channels";
-  String OUTPUT_FRAMES = "output.frames";
-  String OUTPUT_MIN = "output.min";
-  String OUTPUT_MAX = "output.max";
-  String OUTPUT_MEAN = "output.mean";
-  String OUTPUT_RMS = "output.rms";
-  String OUTPUT_CHANNEL_ZERO_PREVIEW = "output.preview.channel0.hex";
-  String CANCELLED_AT_NODE = "cancelledAtNode";
-  String FAILED_NODE_ID = "failure.nodeId";
-  String FAILED_NODE_TYPE = "failure.nodeType";
-  String FAILURE_CLASS = "failure.class";
-  String FAILURE_MESSAGE = "failure.message";
-  String SKIPPED_NODE_IDS = "failure.skippedNodeIds";
+  public static final String BACKEND_MODE = "backendMode";
+  public static final String BACKEND_VERSION = "backendVersion";
+  public static final String OUTPUT_NODE_ID = "output.nodeId";
+  public static final String OUTPUT_PORT_ID = "output.portId";
+  public static final String OUTPUT_DIGEST_ALGORITHM = "output.digest.algorithm";
+  public static final String OUTPUT_DIGEST_ENCODING = "output.digest.encoding";
+  public static final String OUTPUT_DIGEST_SHA256 = "output.digest.sha256";
+  public static final String OUTPUT_SAMPLE_RATE_HZ = "output.sampleRateHz";
+  public static final String OUTPUT_CHANNELS = "output.channels";
+  public static final String OUTPUT_FRAMES = "output.frames";
+  public static final String OUTPUT_MIN = "output.min";
+  public static final String OUTPUT_MAX = "output.max";
+  public static final String OUTPUT_MEAN = "output.mean";
+  public static final String OUTPUT_RMS = "output.rms";
+  public static final String OUTPUT_CHANNEL_ZERO_PREVIEW = "output.preview.channel0.hex";
+  public static final String CANCELLED_AT_NODE = "cancelledAtNode";
+  public static final String FAILED_NODE_ID = "failure.nodeId";
+  public static final String FAILED_NODE_TYPE = "failure.nodeType";
+  public static final String FAILURE_CLASS = "failure.class";
+  public static final String FAILURE_MESSAGE = "failure.message";
+  public static final String SKIPPED_NODE_IDS = "failure.skippedNodeIds";
+
+  private DeterministicAudioArtifacts() {
+    throw new UnsupportedOperationException("Utility class");
+  }
 }
