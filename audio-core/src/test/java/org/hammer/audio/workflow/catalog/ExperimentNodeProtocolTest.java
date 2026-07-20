a5bb20b2d644ac4651bad1ff9e9a9ca9c3ba9d93
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-20 23:05:48 +0200

    Apply project formatting to experiment protocol test (#274)

--- audio-core/src/test/java/org/hammer/audio/workflow/catalog/ExperimentNodeProtocolTest.java
+++ audio-core/src/test/java/org/hammer/audio/workflow/catalog/ExperimentNodeProtocolTest.java
@@ -13,7 +13,8 @@ class ExperimentNodeProtocolTest {
     Node gain = ExperimentNodeCatalog.gain("node.gain");
 
     assertEquals(ExperimentNodeProtocol.TYPE_SYNTHETIC_SIGNAL_GENERATOR, generator.type());
-    assertEquals(ExperimentNodeProtocol.SIGNAL_OUTPUT_PORT, generator.outputPorts().getFirst().id());
+    assertEquals(
+        ExperimentNodeProtocol.SIGNAL_OUTPUT_PORT, generator.outputPorts().getFirst().id());
     assertEquals(ExperimentNodeProtocol.TYPE_GAIN, gain.type());
     assertEquals(ExperimentNodeProtocol.AUDIO_INPUT_PORT, gain.inputPorts().getFirst().id());
     assertEquals(ExperimentNodeProtocol.AUDIO_OUTPUT_PORT, gain.outputPorts().getFirst().id());
