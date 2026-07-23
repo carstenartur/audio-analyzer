fe2aeffc709351b8a2b1e0236da3333a4cfa585b
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-23 16:38:00 +0200

    Model audio subscriptions as explicit registrations

--- audio-app/src/main/java/org/hammer/audio/AudioBlockSubscription.java
+++ audio-app/src/main/java/org/hammer/audio/AudioBlockSubscription.java
@@ -1,10 +1,9 @@
 package org.hammer.audio;
 
 /** Removable subscription to an {@link AudioCaptureService}'s complete audio-block stream. */
-public interface AudioBlockSubscription extends AutoCloseable {
+public interface AudioBlockSubscription {
 
   /** Remove the listener. Safe to call repeatedly. */
-  @Override
   void close();
 
   /** Returns whether this subscription has already been removed. */
